a6a4d24f227abe561cf0edb191fa43320e393a0a
Kyungbae Min <kbmin1129@gmail.com>
2022-09-24 15:23:15 -0400

    Create, Ranking page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import MyPage from "./pages/mypage/MyPage";
+import Ranking from "./pages/ranking/Ranking";
+
+const Layout = () => {};
 
 function App() {
 	return (
@@ -8,6 +11,7 @@ function App() {
 			<Routes>
 				<Route index path="/" element={<Home />} />
 				<Route index path="/user" element={<MyPage />} />
+				<Route index path="/ranking" element={<Ranking />} />
 				{/* <Route path="/request/:reqId" element={<RequestDetail />} /> */}
 				{/* <Route path="*" element={<NotFound />} /> */}
 			</Routes>
