fd54b9af32cca1c2dc9d780c33097f6e092a4567
Kyungbae Min <kbmin1129@gmail.com>
2022-09-24 15:59:28 -0400

    Create, podium and steps

---
+++ src/pages/ranking/PodiumStep.jsx
@@ -0,0 +1,77 @@
+import { motion } from "framer-motion";
+
+export default function PodiumStep({ podium, winner }) {
+	const offset = podium.length - winner.position;
+
+	return (
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				placeContent: "center",
+			}}
+		>
+			<motion.div
+				style={{
+					alignSelf: "center",
+					marginBottom: ".25rem",
+				}}
+				initial="hidden"
+				animate="visible"
+				variants={{
+					visible: {
+						opacity: 1,
+						transition: {
+							delay: 1 + (offset + 2),
+							duration: 0.75,
+						},
+					},
+					hidden: { opacity: 0 },
+				}}
+			>
+				<img
+					src={`https://i.pravatar.cc/64?u=${winner.id}`}
+					alt=""
+					style={{
+						width: "2.75rem",
+						overflow: "hidden",
+						height: "2.75rem",
+						borderRadius: 9999,
+					}}
+				/>
+			</motion.div>
+
+			<motion.div
+				style={{
+					width: "4rem",
+					placeContent: "center",
+					display: "flex",
+					borderTopLeftRadius: ".5rem",
+					borderTopRightRadius: ".5rem",
+					borderColor: "rgba(190,24,93,1)",
+					backgroundColor: "rgba(219,39,119,1)",
+					marginBottom: -1,
+					filter: `opacity(${0.1 + offset / podium.length})`,
+				}}
+				initial="hidden"
+				animate="visible"
+				variants={{
+					visible: {
+						height: 200 * (offset / podium.length),
+						opacity: 1,
+						transition: {
+							delay: 1 + offset,
+							duration: 2,
+							ease: "backInOut",
+						},
+					},
+					hidden: { opacity: 0, height: 0 },
+				}}
+			>
+				<span style={{ color: "white", alignSelf: "flex-end" }}>
+					{winner.position + 1}
+				</span>
+			</motion.div>
+		</div>
+	);
+}
--- src/pages/ranking/Ranking.jsx
+++ src/pages/ranking/Ranking.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Grid, IconButton, Typography } from "@mui/material";
+import BottomNav from "../../components/bottomNav/BottomNav";
+import ArrowBackRounded from "@mui/icons-material/ArrowBackRounded";
+import SettingsRounded from "@mui/icons-material/Settings";
 
 const Ranking = () => {
-	return <div>Ranking</div>;
+	return (
+		<Grid>
+			{/* HEADER */}
+			<Grid
+				container
+				alignItems={"center"}
+				justifyContent={"space-between"}
+				height={"50px"}
+			>
+				<IconButton>
+					<ArrowBackRounded />
+				</IconButton>
+				<Typography>Ranking</Typography>
+				<IconButton>
+					<SettingsRounded />
+				</IconButton>
+			</Grid>
+
+			{/*  */}
+			<BottomNav />
+		</Grid>
+	);
 };
 
 export default Ranking;
