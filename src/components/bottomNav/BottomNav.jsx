da9b637432f923395f1b99669224d3ca6bfe02d7
Kyungbae Min <kbmin1129@gmail.com>
2022-09-24 22:42:51 -0400

    UPDATE, boxshadow added

--- src/components/bottomNav/BottomNav.jsx
+++ src/components/bottomNav/BottomNav.jsx
@@ -1,41 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import { mobileMaxWidthMediaQuery } from "../../theme";
+import { useNavigate } from "react-router-dom";
+
+// import useMediaQuery from "@mui/material/useMediaQuery";
+// import { mobileMaxWidthMediaQuery } from "../../theme";
 
 import EmojiEventsRoundedIcon from "@mui/icons-material/EmojiEventsRounded";
 import RollerSkatingRoundedIcon from "@mui/icons-material/RollerSkatingRounded";
 import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
 
 const BottomNav = () => {
-	const [value, setValue] = React.useState("recents");
-	const isMobile = useMediaQuery(mobileMaxWidthMediaQuery);
+	const navigate = useNavigate();
 
 	const handleChange = (event, newValue) => {
-		setValue(newValue);
+		navigate(`${newValue}`);
 	};
 
 	return (
 		<>
 			<BottomNavigation
-				sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
-				value={value}
+				sx={{
+					position: "fixed",
+					bottom: 0,
+					left: 0,
+					right: 0,
+					// color: "E0E0E0",
+					boxShadow: 3,
+				}}
+				elevation={1}
+				value={window.location.pathname}
 				onChange={handleChange}
 			>
 				<BottomNavigationAction
 					label="ranking"
-					value="ranking"
+					value="/ranking"
 					icon={<EmojiEventsRoundedIcon />}
 				/>
 				<BottomNavigationAction
 					label="run"
-					value="run"
+					value="/"
 					icon={<RollerSkatingRoundedIcon />}
 				/>
 				<BottomNavigationAction
 					label="profile"
-					value="profile"
+					value="/user"
 					icon={<PersonRoundedIcon />}
 				/>
 			</BottomNavigation>
